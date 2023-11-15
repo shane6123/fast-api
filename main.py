2007294a5f93871f02802fababdca92d4a1a2e14
Shane Alam <shanealam6123@gmail.com>
2023-11-15 20:06:55 +0530

    done

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+# get route for test 
+@app.get("/test")
+async def test():
+    return {"message": "Hello for test"}
+
+# post route for test and return body data 
+@app.post("/test")
+async def test(data: dict):
+    return {"message": data}
+
+
